ed3a49ab497224de50f573e59f9be0e059ed5b1c
sssBHub <clawdspaws@gmail.com>
2026-08-18 12:28:41 +0200

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,64 @@
+return {
+    state = {
+        autoTrain = false,
+        autoRebirth = false,
+        rebirthLimit = false,
+        rebirthTarget = 1000,
+
+        autoJungleRock = false,
+        autoEgg = false,
+        autoUltimates = false,
+
+        autoSize = false,
+        autoSpeed = false,
+        sizeMode = "Max",
+        speedMode = "Max",
+        sizeCustom = 2,
+        speedCustom = 93,
+
+        skipRebirthAnimation = false,
+        antiAFK = false,
+
+        esp = false,
+        espBoxes = true,
+        espNames = true,
+        espDistance = true,
+        espHealth = true,
+        espTracers = false,
+        espTeamCheck = false,
+        espMaxDistance = 1500,
+
+        coords = false,
+        coordsCompass = true,
+        coordsHeading = true,
+        coordsPitch = true,
+
+        automationOverlay = true,
+        performanceOverlay = true,
+
+        notifications = true,
+        petNotifications = true,
+        auraNotifications = true,
+        rarityNotifications = true,
+
+        rareBasic = false,
+        rareRare = false,
+        rareEpic = true,
+        rareUnique = true,
+        rareAdvanced = true,
+
+        spyAutoRefresh = true,
+        muteStrength = false,
+        muteRebirth = false,
+    },
+
+    goal = {
+        type = "Strength",
+        target = 100000000,
+        enabled = false,
+    },
+
+    hotkeys = {},
+    selectedPets = {},
+    selectedAuras = {},
+}
